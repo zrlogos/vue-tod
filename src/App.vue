7d665ea5e94121dfbcb1b9e8137cb9981a90bd53
z <2300549455@qq.com>
2024-03-03 13:55:36 +0800

    a

--- src/App.vue
+++ src/App.vue
@@ -14,68 +14,64 @@ export default {
 }
 </script>
 
+
 <template>
-    <nav class="navbar">
-      <router-link class="nav-link" to="/main">主页</router-link>
-      <router-link class="nav-link" to="/point-info">站点信息</router-link>
-      <router-link class="nav-link" to="/beijing-subway">北京地铁</router-link>
-      <router-link class="nav-link" to="/line">廊道TOD介绍</router-link>
-    </nav>
-    <div class="content">
-      <router-view v-slot="{Component}">
-        <transition>
-          <keep-alive>
-            <component :is="Component" />
-          </keep-alive>
-        </transition>
-      </router-view>
-      <div></div>
-    </div>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <el-menu
+            :default-active="activeIndex"
+            class="el-menu-demo"
+            mode="horizontal"
+            :ellipsis="false"
+        >
+          <div class="flex-grow"/>
+          <router-link class="nav-link" to="/main">
+            <el-menu-item index="1">主页</el-menu-item>
+          </router-link>
+          <router-link class="nav-link" to="/point-info">
+            <el-menu-item index="2">站点信息</el-menu-item>
+          </router-link>
+          <router-link class="nav-link" to="/beijing-subway">
+            <el-menu-item index="3">北京地铁</el-menu-item>
+          </router-link>
+          <router-link class="nav-link" to="/line">
+            <el-menu-item index="4">廊道TOD介绍</el-menu-item>
+          </router-link>
+        </el-menu>
+      </el-header>
+      <el-main>
+        <div>
+          <router-view v-slot="{Component}">
+            <transition>
+              <keep-alive>
+                <component :is="Component"/>
+              </keep-alive>
+            </transition>
+          </router-view>
+          <div></div>
+        </div>
+
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
+
 <style>
-.navbar {
-  background-color: #214b5b;
-  color: #fff;
-  width: 100%;
-  height: 40px;
-  position: fixed;
-  top: 0;
-  left: 0;
-}
-.navbar .nav-link {
+.nav-link {
   margin-right: 15px;
-  font-size: 20px;
-  color: #fff;
   text-decoration: none;
-  padding: 10px;
-  font-weight: bold;
-  font-family:    Helvetica, sans-serif;
+  font-weight: 700;
+  font-family: Inter, 'Helvetica Neue', Helvetica, 'PingFang SC',
+  'Hiragino Sans GB', 'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
 }
-.content {
-  margin-top: 40px;
+
+body, html {
+  height: 100%;
+  margin: 0;
   padding: 0;
-  width: 100%;
-  height: calc(100% - 40px);
-  position: fixed;
 }
-html, body {
-  width: 100%;
-  height: 100%;
-  font-size: 14px;
-  line-height: 1.6;
-  color: #636b6f;
-}
-
-body {
-  position: relative;
-}
-
-#app {
-  width: 100%;
-  height: 100%;
-}
-
 
 
 </style>
